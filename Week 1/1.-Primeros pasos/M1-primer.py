afb24e56c5db8f50df58c1ce1633392a75326ec1
Michael Salgado Sotero <80673026+mssj-11@users.noreply.github.com>
2022-02-14 00:30:44 -0600

    Add files via upload

---
+++ Week 1/1.-Primeros pasos/M1-primer.py
@@ -0,0 +1,11 @@
+
+#PRIMER PROGRAMA
+print("Hi , this is my first program in Python")
+
+from datetime import date
+print("Today's date is: " + str(date.today()))
+
+
+parsec = 11
+lightyears = 3.26156 * parsec
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
